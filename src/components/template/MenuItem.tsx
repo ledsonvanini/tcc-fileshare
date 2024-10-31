c5527be5e75a37263dc4274c743e1df7635a7159
Ledson Vanini <ledsonvanini@gmail.com>
2024-10-31 01:16:09 +0000

    fix: Login firebase11 - Ok

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -7,35 +7,27 @@ interface MenuItemProps {
     className?: string
     onClick?: (evento: any) => void
 }
-
 export default function MenuItem(props: MenuItemProps) {
     function renderizarLink() {
         return (
-            <a className={`
-                    flex flex-col justify-center items-center
-                    h-20 w-20
-                    dark:text-gray-200
-                    ${props.className}
-                `}>
+            <div className={`flex flex-col justify-center items-center h-20 w-20 dark:text-gray-200 ${props.className}`}>
                 {props.icone}
                 <span className={`text-xs font-light`}>
                     {props.texto}
                 </span>
-            </a>
-        )
+            </div>
+        );
     }
+
     return (
-        <li onClick={props.onClick} className={`
-            hover:bg-gray-100 dark:hover:bg-gray-800
-            cursor-pointer
-        `}>
+        <li onClick={props.onClick} className={`hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer`}>
             {props.url ? (
-                <Link href={props.url}>
+                <Link href={props.url} className="flex flex-col">
                     {renderizarLink()}
                 </Link>
             ) : (
                 renderizarLink()
             )}
         </li>
-    )
+    );
 }
